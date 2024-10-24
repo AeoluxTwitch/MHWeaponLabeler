6597d55a1c6a1100cc42d0e5491c2b5c35df86a9
AeoluxTwitch <85392835+AeoluxTwitch@users.noreply.github.com>
2024-10-24 15:48:52 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,10 @@
 import { Label } from './types.js';
 
 export const DELETE = 'insert-rkey-of-delete-post-here';
-export const LABEL_LIMIT = 1;
+export const LABEL_LIMIT = 2;
 export const LABELS: Label[] = [
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b37gydsg26',
     identifier: 'longsword',
     locales: [
       { lang: 'en', name: 'Long Sword', description: 'Long Sword'},
@@ -25,77 +25,77 @@ export const LABELS: Label[] = [
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5gtq2yu26',
     identifier: 'swordandshield',
     locales: [
       { lang: 'en', name: 'Sword and Shield', description: 'Sword and Shield'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5h2n3ss2x',
     identifier: 'dualblades',
     locales: [
       { lang: 'en', name: 'Dual Blades', description: 'Dual Blades'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5hamfv22x',
     identifier: 'hammer',
     locales: [
       { lang: 'en', name: 'Hammer', description: 'Hammer'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5hii3pm23',
     identifier: 'huntinghorn',
     locales: [
       { lang: 'en', name: 'Hunting Horn', description: 'Hunting Horn'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5horc3m2f',
     identifier: 'lance',
     locales: [
       { lang: 'en', name: 'Lance', description: 'Lance'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5hwxcu42f',
     identifier: 'gunlance',
     locales: [
       { lang: 'en', name: 'Gunlance', description: 'Gunlance'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5isurt42h',
     identifier: 'insectglaive',
     locales: [
       { lang: 'en', name: 'Insect Glaive', description: 'Insect Glaive'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5gkzc6o2z',
     identifier: 'greatsword',
     locales: [
       { lang: 'en', name: 'Great Sword', description: 'Great Sword'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5izege42h',
     identifier: 'lightbowgun',
     locales: [
       { lang: 'en', name: 'Light Bowgun', description: 'Light Bowgun'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5j6esys2x',
     identifier: 'heavybowgun',
     locales: [
       { lang: 'en', name: 'Heavy Bowgun', description: 'Heavy Bowgun'},
     ]
   },
   {
-    rkey: 'insert-rkey-here',
+    rkey: '3l7b5jcl4r42g',
     identifier: 'bow',
     locales: [
       { lang: 'en', name: 'Bow', description: 'Bow'},
